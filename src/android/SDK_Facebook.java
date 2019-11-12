9b532efa027d16c7f356cee66c475e82af3917b8
Jaime Cabello Venegas <jai.cabellov@gmail.com>
2019-11-12 12:31:20 -0300

    changes
    changes con SDK_Facebook.java

--- src/android/SDK_Facebook.java
+++ src/android/SDK_Facebook.java
@@ -15,20 +15,15 @@ import com.facebook.appevents.AppEventsLogger;
 public class SDK_Facebook extends CordovaPlugin {
 
     @Override
-    public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
-        if (action.equals("coolMethod")) {
-            String message = args.getString(0);
-            this.coolMethod(message, callbackContext);
-            return true;
-        }
-        return false;
+    public boolean logEventSDKFacebook(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
+        AppEventsLogger logger = AppEventsLogger.newLogger(this.cordova.getActivity());
+        //event name that we want to track
+         if (action.equals("logEvent")) {
+             logger.logEvent(AppEventsConstants.EVENT_NAME_VIEWED_CONTENT);
+             return true;
+         }
+         return false;
     }
 
-    private void coolMethod(String message, CallbackContext callbackContext) {
-        if (message != null && message.length() > 0) {
-            callbackContext.success(message);
-        } else {
-            callbackContext.error("Expected one non-empty string argument.");
-        }
-    }
+
 }
